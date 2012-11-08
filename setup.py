ac0617845d9c288018fd95748aed28cabeab5e70
Peter Williams <pwil3058@bigpond.net.au>
2012-11-08 17:09:05 +1000

    distribution: start setting up the distribution mechanism
    Signed-off-by: Peter Williams <pwil3058@bigpond.net.au>

---
+++ setup.py
@@ -0,0 +1,70 @@
+#! /bin/env python
+### Copyright: Peter Williams (2012) - All rights reserved
+###
+### This program is free software; you can redistribute it and/or modify
+### it under the terms of the GNU General Public License as published by
+### the Free Software Foundation; version 2 of the License only.
+###
+### This program is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+### GNU General Public License for more details.
+###
+### You should have received a copy of the GNU General Public License
+### along with this program; if not, write to the Free Software
+### Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+from distutils.core import setup
+
+NAME = 'PaintersColourAssistant'
+
+VERSION = '0.01'
+
+DESCRIPTION = 'A set of tools for painters to experiment with mixing colours.'
+
+LONG_DESCRIPTION =\
+'''
+This software is a set of tools for painters who wish to experiment with
+mixing paints and plan palettes (electronically) before beginning a
+painting.
+'''
+
+LICENSE = 'GNU General Public License (GPL) Version 2.0'
+
+CLASSIFIERS = [
+    'Development Status :: Pre-Alpha',
+    'Intended Audience :: End Users/Desktop',
+    'License :: OSI Approved :: %s' % LICENSE,
+    'Programming Language :: Python',
+    'Topic :: Artistic Software',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+]
+
+AUTHOR = 'Peter Williams'
+
+AUTHOR_EMAIL = 'pwil3058@bigpond.net.au'
+
+URL = 'http://sourceforge.net/projects/pcatk/'
+
+SCRIPTS = ['pcatk_palette.py', 'pcatk_editor.py']
+
+PACKAGES = ['pcatk']
+
+DESKTOP = [('share/applications', ['pcatk_editor.desktop', 'pcatk_palette.desktop'])]
+
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    classifiers = CLASSIFIERS,
+    license = LICENSE,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = URL,
+    scripts = SCRIPTS,
+    packages = PACKAGES,
+    data_files = DESKTOP
+)
