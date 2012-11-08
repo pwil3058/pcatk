d870903ac3ccd2daf1ad31eeeb2ab782302b42c3
Peter Williams <pwil3058@bigpond.net.au>
2012-11-08 17:07:16 +1000

    copyright: add my name
    Signed-off-by: Peter Williams <pwil3058@bigpond.net.au>

--- pcatk_palette.py
+++ pcatk_palette.py
@@ -1,4 +1,5 @@
 #! /bin/env python
+### Copyright: Peter Williams (2012) - All rights reserved
 ###
 ### This program is free software; you can redistribute it and/or modify
 ### it under the terms of the GNU General Public License as published by
